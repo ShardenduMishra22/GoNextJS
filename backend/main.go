5242ba4fe4b60d5893245f318a9adccc729c8df4
Shardendu Mishra <shardendumishra01@gmail.com>
2024-11-07 01:59:53 +0530

    Backend Done

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,231 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Backend Service in GoLang")
+
+	// Connect to the database
+	db := ConnectDatabase()
+	defer db.Close()
+
+	// CREATE a table in the database
+	CreateTable(db)
+
+	// Setup routes and server
+	router := mux.NewRouter()
+	router.Handle("/", EnableCORS(http.HandlerFunc(homeHandler)))
+
+	// Test Route - Start
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]string{"Message": "Welcome to the Backend Service in Go!"}
+		json.NewEncoder(w).Encode(response)
+	})
+	// Test Route - End
+
+	// Routes for the API - Start
+	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users", createUsers(db)).Methods("POST")
+	router.HandleFunc("/api/go/users/{id}", getUsersId(db)).Methods("GET")
+	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
+	// Routes for the API - End
+
+	// Start the HTTP server
+	ListenAndServe(router)
+}
+
+// Delete a user
+func deleteUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		_, err := db.Exec("DELETE FROM users WHERE id=$1", id)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+// Update a user by Id
+func updateUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		_, err := db.Exec("UPDATE users SET name=$1, email=$2 WHERE id=$3", user.Name, user.Email, id)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var updatedUser User
+		err = db.QueryRow("SELECT id, name, email FROM users WHERE id=$1", id).Scan(&updatedUser.Id, &updatedUser.Name, &updatedUser.Email)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(updatedUser)
+	}
+}
+
+// Create a new user
+func createUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+		err := db.QueryRow("INSERT INTO users (name, email) VALUES ($1,$2) RETURNING id",user.Name,user.Email).Scan(&user.Id)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(user)
+	}
+}
+
+// Get a user by Id
+func getUsersId(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var user User
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&user.Id, &user.Name, &user.Email)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		json.NewEncoder(w).Encode(user)
+	}
+}
+
+// Get all users
+func getUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM users")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		users := []User{}
+
+		for rows.Next() {
+			var user User
+			err := rows.Scan(&user.Id, &user.Name, &user.Email)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, user)
+		}
+
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(users)
+	}
+}
+
+// Listen to the server
+func ListenAndServe(handler http.Handler) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not specified
+	}
+
+	log.Println("Starting server on port:", port)
+	err := http.ListenAndServe(":"+port, handler)
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+}
+
+// CORS middleware
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Home handler example
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Backend Service in Go!")
+}
+
+// Database table creation
+func CreateTable(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+	if err != nil {
+		log.Printf("Error creating table: %v", err)
+	}
+}
+
+// Database connection
+func ConnectDatabase() *sql.DB {
+	// Ensure that SSL mode is disabled (when using Docker)
+	connStr := os.Getenv("DATABASE_URL") + "?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Could not establish a connection with the database:", err)
+	}
+	return db
+}
+
+// User struct
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
